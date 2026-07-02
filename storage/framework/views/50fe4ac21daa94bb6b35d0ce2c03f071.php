c672c2ed3e0baf806fc1607dd5e9aa258bec6c27
Dleiszar Jeisalther S. Lagariza <dleiszarjeisaltherlagariza@gmail.com>
2026-07-02 10:48:10 +0800

    register now to Today

---
+++ storage/framework/views/50fe4ac21daa94bb6b35d0ce2c03f071.php
@@ -0,0 +1,6 @@
+
+<button <?php echo e($attributes->merge(['type' => 'submit', 'class' => config('ui.buttons.primary')])); ?>>
+    <?php echo e($slot); ?>
+
+</button>
+<?php /**PATH C:\Users\universal\Herd\my-first-app\resources\views/components/buttons/primary-button.blade.php ENDPATH**/ ?>
